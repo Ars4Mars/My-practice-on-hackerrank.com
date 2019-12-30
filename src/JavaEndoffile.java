bab5b18394634c6b37426baf71779050d29b74f1
ArsMars <tag_mahal@mail.ru>
2019-12-30 07:51:20 +0300

    inizializerofBlocks

---
+++ src/JavaEndoffile.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class JavaEndoffile {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int count = 1;
+
+        while(s.hasNext()) {
+            String ns = s.nextLine();
+            System.out.println(count + " " + ns);
+            count++;
+        }
+    }
+}
